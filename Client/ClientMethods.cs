215c593d5d387f80d677dace9fac7d297121ee35
Ikthios <joshua.abrams@gmx.us>
2015-10-25 18:17:40 -0500

    Moved methods into new class.

---
+++ Client/ClientMethods.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace Client
+{
+    class ClientMethods
+    {
+        public void SendFile(string port, string file, string peerIp)
+        {
+            // Internal check
+            Debug.WriteLine("Constructing file [CLIENTTHREADHANDLER]");
+            // Create the requested file
+            string filePath = @"C:\Clinet\";
+            string fileName = file;
+            byte[] fnByte = Encoding.ASCII.GetBytes(fileName);
+            byte[] fileData = File.ReadAllBytes(filePath + fileName);
+            byte[] clientData = new byte[4 + fnByte.Length + fileData.Length];
+            byte[] fileNameLength = BitConverter.GetBytes(fnByte.Length);
+            fileNameLength.CopyTo(clientData, 0);
+            fnByte.CopyTo(clientData, 4);
+            fileData.CopyTo(clientData, 4 + fnByte.Length);
+
+            try
+            {
+                Socket TcpPeerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect to the peer
+                TcpPeerSocket.Connect(peerIp, int.Parse(port));
+                // Send the file to the requesting peer
+                Debug.WriteLine("Sending file " + file + " to " + peerIp + ":" + port + " [CLIENTTHREADHANDLER]");
+                TcpPeerSocket.Send(clientData);
+                try
+                {
+                    // Close the peer socket after the data has been sent
+                    Debug.WriteLine("Killing TcpPeerSocket [CLIENTTHREADHANDER].");
+                    TcpPeerSocket.Close();
+                }
+                catch (Exception error)
+                {
+                    Debug.WriteLine("TcpPeerSocket.Close() error [CLIENTTHREADHANDLER] " + error.ToString());
+                }
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Sending file error: " + error.ToString() + " [CLIENTTHREADHANDLER]");
+            }
+        }
+
+        public void ReceiveFile(Socket TcpClientSocket, byte[] dataArray, int csr)
+        {
+            // Client receives requested file fron TcpClientSocket data variable 'csr'
+            /*
+            byte[] b = new byte[1500];
+            int csr = TcpClientSocket.Receive(b);
+            */
+            try
+            {
+                // Receive the requested file
+                Debug.WriteLine("Receiving requested file.");
+
+                string receivedPath = @"C:/Clinet/";
+                int fileNameLen = BitConverter.ToInt32(dataArray, 0);
+                string fileName = Encoding.ASCII.GetString(dataArray, 4, fileNameLen);
+                BinaryWriter bWrite = new BinaryWriter(File.Open(receivedPath + fileName, FileMode.Create/*FileMode.Append, FileAccess.Write*/));
+                bWrite.Write(dataArray, 4 + fileNameLen, csr - 4 - fileNameLen);
+
+                try
+                {
+                    // Kill the BinaryWriter and close the socket after the file has been received.
+                    Debug.WriteLine("Killing the BinaryWriter [CLIENTTHREADHANDER].");
+                    bWrite.Close();
+                    TcpClientSocket.Close();
+                    //loop = false;
+                }
+                catch (Exception error)
+                {
+                    Debug.WriteLine("BinaryWriter kill error [CLIENTTHREADHANDLER] " + error.ToString());
+                }
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("File receive error [CLIENTTHREADHANDLER] " + error.ToString());
+            }
+        }
+    }
+}
--- Client/ClientThreadHandler.cs
+++ Client/ClientThreadHandler.cs
@@ -16,7 +16,7 @@ namespace Client
     {
         // Global variables
         private Socket TcpClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private Socket TcpPeerSocket;
+        ClientMethods cm = new ClientMethods();
 
         // Default constructor
         public ClientThreadHandler() { }
@@ -69,79 +69,25 @@ namespace Client
                 */
                 if (tokens[0].Equals("PPR"))
                 {
-                    // Internal check
-                    Debug.WriteLine("Constructing file [CLIENTTHREADHANDLER]");
-                    // Create the requested file
-                    string filePath = @"C:\Clinet\";
-                    string fileName = tokens[3];
-                    byte[] fnByte = Encoding.ASCII.GetBytes(fileName);
-                    byte[] fileData = File.ReadAllBytes(filePath + fileName);
-                    byte[] clientData = new byte[4 + fnByte.Length + fileData.Length];
-                    byte[] fileNameLength = BitConverter.GetBytes(fnByte.Length);
-                    fileNameLength.CopyTo(clientData, 0);
-                    fnByte.CopyTo(clientData, 4);
-                    fileData.CopyTo(clientData, 4 + fnByte.Length);
-
                     try
                     {
-                        TcpPeerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-                        // Connect to the peer
-                        TcpPeerSocket.Connect(tokens[4], int.Parse(tokens[2]));
-                        // Send the file to the requesting peer
-                        Debug.WriteLine("Sending file " + tokens[3] + " to " + tokens[4] + ":" + tokens[2] + " [CLIENTTHREADHANDLER]");
-                        TcpPeerSocket.Send(clientData);
-                        try
-                        {
-                            // Close the peer socket after the data has been sent
-                            Debug.WriteLine("Killing TcpPeerSocket [CLIENTTHREADHANDER].");
-                            TcpPeerSocket.Close();
-                            loop = false;
-                        }
-                        catch(Exception error)
-                        {
-                            Debug.WriteLine("TcpPeerSocket.Close() error [CLIENTTHREADHANDLER] " + error.ToString());
-                        }
-                    }catch(Exception error)
+                        cm.SendFile(tokens[2], tokens[3], tokens[4]);
+                    }
+                    catch(Exception error)
                     {
-                        Debug.WriteLine("Sending file error: " + error.ToString() + " [CLIENTTHREADHANDLER]");
+                        Debug.WriteLine("File send error: " + error.ToString());
                     }
                 }
                 else
                 {
-                    // Client receives requested file fron TcpClientSocket data variable 'csr'
-                    /*
-                    byte[] b = new byte[1500];
-                    int csr = TcpClientSocket.Receive(b);
-                    */
                     try
                     {
-                        // Receive the requested file
-                        Debug.WriteLine("Receiving requested file.");
-
-                        string receivedPath = @"C:/Clinet/";
-                        int fileNameLen = BitConverter.ToInt32(dataArray, 0);
-                        string fileName = Encoding.ASCII.GetString(dataArray, 4, fileNameLen);
-                        BinaryWriter bWrite = new BinaryWriter(File.Open(receivedPath + fileName, FileMode.Create/*FileMode.Append, FileAccess.Write*/));
-                        bWrite.Write(dataArray, 4 + fileNameLen, csr - 4 - fileNameLen);
-
-                        try
-                        {
-                            // Kill the BinaryWriter and close the socket after the file has been received.
-                            Debug.WriteLine("Killing the BinaryWriter [CLIENTTHREADHANDER].");
-                            bWrite.Close();
-                            TcpClientSocket.Close();
-                            //loop = false;
-                        }
-                        catch(Exception error)
-                        {
-                            Debug.WriteLine("BinaryWriter kill error [CLIENTTHREADHANDLER] " + error.ToString());
-                        }
+                        cm.ReceiveFile(TcpClientSocket, dataArray, csr);
+                        loop = false;
                     }
                     catch(Exception error)
                     {
-                        Debug.WriteLine("File receive error [CLIENTTHREADHANDLER] " + error.ToString());
-                        loop = false;
+                        Debug.WriteLine("File receive error: " + error.ToString());
                     }
                 }
             }   // End while loop
